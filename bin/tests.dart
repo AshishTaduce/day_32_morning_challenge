4ca20cccf0e65e8a591c514cd24738fe0a2ec79b
Varun Resh Maddali <mvresh@gmail.com>
2019-11-18 11:45:56 +0530

    new test cases again

--- bin/tests.dart
+++ bin/tests.dart
@@ -7,14 +7,30 @@ test('Shiritori 1', (){
 Shiritori game = Shiritori();
 expect(game.words.isEmpty, true);
 expect(game.play('hello'), true);
-expect(game.words.isEmpty, false);
+expect(game.words, ['hello']);
 expect(game.play('nice'), false);
+expect(game.game_over,true);
 expect(game.restart(), "game restarted");
 expect(game.play('hello'), true);
+expect(game.game_over,false);
 expect(game.words.isEmpty, false);
 expect(game.play('old'), true);
 expect(game.words.isEmpty, false);
 expect(game.play('demo'), true);
 expect(game.words.isEmpty, false);
-expect(game.play('nice'), false);
+expect(game.play('old'), false);
+expect(game.restart(), "game restarted");
+expect(game.play('hello'), true);
+expect(game.game_over,false);
+expect(game.words.isEmpty, false);
+expect(game.play('old'), true);
+expect(game.words.isEmpty, false);
+expect(game.play('demo'), true);
+expect(game.words.isEmpty, false);
+expect(game.play('oldie'), true);
+expect(game.play('empty'), true);
+expect(game.play('yes'), true);
+expect(game.play('yes'), false);
+
+
 });}
